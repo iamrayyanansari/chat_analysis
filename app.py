a5fc77959199265897bdcdfd85ee3d5985d3a9e1
sohel <pathansohel415@gmail.com>
2023-07-22 16:31:49 +0530

    complete WhatsApp chat analyser

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import preprocessor,helper
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title("WhatsApp Chat Analyser")
 
@@ -23,14 +24,107 @@ if uploaded_file is not None:
 
 
     if st.sidebar.button("Show Analysis"):
-        num_messages,words =helper.fetch_stats(selected_user,df)
+        num_messages,words,num_media_messages,num_links =helper.fetch_stats(selected_user,df)
+        st.title("Top Statistics")
         col1,col2,col3,col4 =st.columns(4)
+
         with col1:
             st.header("Total Message")
             st.title(num_messages)
         with col2:
             st.header("Total Words")
             st.title(words)
+
+        with col3:
+            st.header("Total Media message")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Total Number of links")
+            st.title(num_links)
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'],color ='green')
+        plt.xticks(rotation= 'vertical')
+        st.pyplot(fig)
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+        # activity map
+        st.title("Activity Map")
+        col1,col2 = st.columns(2)
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map(selected_user,df)
+            fig,ax = plt.subplots()
+            ax.bar(busy_day.index,busy_day.values)
+            st.pyplot(fig)
+        with col2:
+            st.header("Most busy Month")
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values,color = "orange")
+            plt.xticks(rotation = "vertical")
+            st.pyplot(fig)
+        st.title("Weekly activity map")
+        user_heatmap =helper.activity_heatmap(selected_user,df)
+        fig,ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+
+
+        # finding the busiest users in the group
+        if selected_user == 'Overall':
+            st.title('Most Busy Users')
+            x,new_df= helper.most_busy_users(df)
+            fig,ax = plt.subplots()
+
+            col1,col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values,color='red')
+                plt.xticks(rotation = 'vertical')
+                st.pyplot(fig)
+
+            with col2:
+                st.dataframe(new_df)
+        st.title("WordCloud")
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+
+        # most common words
+        most_common_df = helper.most_common_words(selected_user,df)
+
+        st.dataframe(most_common_df)
+        fig,ax = plt.subplots()
+        ax.barh(most_common_df['Word'],most_common_df['Frequency'])
+
+        plt.xticks(rotation = 'vertical')
+
+        st.title("most common words")
+        st.pyplot(fig)
+
+        emoji_df = helper.emoji_helper(selected_user,df)
+        st.title("emoji Analysis")
+        col1,col2 = st.columns(2)
+        with col1:
+            st.dataframe(emoji_df)
+        with col2:
+            fig,ax = plt.subplots()
+            ax.pie(emoji_df['Count'].head(),labels = emoji_df["Emoji"].head(),autopct="%0.2f")
+
+            st.pyplot(fig)
+
+
 
 
 
--- preprocessor.py
+++ preprocessor.py
@@ -26,14 +26,27 @@ def preprocess(data):
     df['message'] = messages
 
     # extract additional date/time columns
+    df['only_date'] = df['message_date'].dt.date
     df['year'] = df['message_date'].dt.year
-    df['month'] = df['message_date'].dt.month
+    df['month_num'] = df['message_date'].dt.month
+    df['month'] = df['message_date'].dt.month_name()
     df['day'] = df['message_date'].dt.day
+    df['day_name'] = df['message_date'].dt.day_name()
     df['hour'] = df['message_date'].dt.hour
     df['minute'] = df['message_date'].dt.minute
 
+    period = []
+    for hour in df[['day_name','hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+    df['period'] = period
+
     # reordering columns
-    df = df[['message_date', 'year', 'month', 'day', 'hour', 'minute', 'user', 'message']]
+    df = df[['period','only_date','message_date', 'year', 'month','month_num', 'day','day_name', 'hour', 'minute', 'user', 'message']]
 
     return df
 
